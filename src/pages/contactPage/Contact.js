ac7a1bf9ff4148df6faff524ca492ba524d75ed1
Jason Gionfriddo <Ragtop66goat@gmail.com>
2022-12-11 17:40:29 -0600

    updated styling

--- src/pages/contactPage/Contact.js
+++ src/pages/contactPage/Contact.js
@@ -13,21 +13,21 @@ export function Contact() {
       <div className="container">
         <div className="contact-container">
           <img src={dog} className="image" />
-          <div className="mx-5">
+          <div className="m-5">
             <h3>Request your dates today</h3>
-            <Form className="contact-form">
-              <Form.Group className="mb-3" controlId="name">
+            <Form className="contact-form bg-light p-3">
+              <Form.Group className="mb-3 text-start" controlId="name">
                 <Form.Label>Name</Form.Label>
                 <Form.Control type="text" placeholder="name" />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Group className="mb-3" controlId="startDate">
+                <Form.Group className="mb-3 text-start" controlId="startDate">
                   <Form.Label>Start Date</Form.Label>
                   <Form.Control type="date" placeholder="start date" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="endDate">
+                <Form.Group className="mb-3 text-start" controlId="endDate">
                   <Form.Label>End Date</Form.Label>
                   <Form.Control type="date" placeholder="end date" />
                 </Form.Group>
