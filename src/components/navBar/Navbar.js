b7a8bca8f8f88658123c80089d3022c37a3e8981
Jason Gionfriddo <Ragtop66goat@gmail.com>
2024-03-09 17:43:38 -0600

    updated NavBar Nav.Link comps to Link comps

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -2,30 +2,31 @@ import logo from "../../images/logo.png";
 import Navbar from "react-bootstrap/Navbar";
 import "./navbar.css";
 import { Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
     <div className="nav-position" data-testid="navbar">
       <Navbar expand="lg" className="nav-bg">
         <Container className="d-flex justify-content-between">
-          <Navbar.Brand to="/">
+          <Navbar.Brand href="/">
             <img src={logo} alt="logo" className="logo" />{" "}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse>
             <Nav className="me-auto">
-              <Nav.Link to="/" className="navLink">
+              <Link to="/" className="navLink">
                 Home
-              </Nav.Link>
-              <Nav.Link to="/seasons" className="navLink">
+              </Link>
+              <Link to="/seasons" className="navLink">
                 Seasons
-              </Nav.Link>
-              <Nav.Link to="/lodging" className="navLink">
+              </Link>
+              <Link to="/lodging" className="navLink">
                 Lodging
-              </Nav.Link>
-              <Nav.Link to="/contact" className="navLink">
+              </Link>
+              <Link to="/contact" className="navLink">
                 Contact
-              </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
